4d8e2bc14d8da5d82c4452db3e5234d96adc13df
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-03 16:41:22 -0500

    MainLayout

---
+++ src/components/HeaderBar/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <b-navbar toggleable type="drak">
+      <b-navbar-brand class="logo" @click="$router.push({name: 'Home'})">BitaTrading</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right v-if="user">
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <b-avatar :src="user.photoURL || ''"></b-avatar>
+            </template>
+            <b-dropdown-item @click="logOut">Cerrar sesión</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <div v-else>
+            <b-button @click="go('SignUp')" class="botones bg-success mr-2">Registrarse</b-button>
+            <b-button @click="go('Login')" class="botones bg-success mr-2">Iniciar sesión</b-button>
+          </div>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .header{
+    background: #5E60CE;
+    .logo{
+      cursor: pointer;
+    }
+    .botones{
+      background: #4EA8DE;
+    }
+  }
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+      <HeaderBar/>
+      <router-view/>
+      <FooterBar/>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar'
+import FooterBar from '@/components/FooterBar'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    HeaderBar,
+    FooterBar
+  }
+}
+</script>
